aae3b229d10aa51f3c1075ad91fa5c91173d8cae
shenolfej <shenolfejzoski@gmail.com>
2026-01-09 17:51:51 +0100

    Create zadaca3.java

---
+++ dopolnitelni_zadaci_za_vezbanje_pred_ispit/zadaca3.java
@@ -0,0 +1,137 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class MapEntry<K extends Comparable<K>,E> implements Comparable<K> {
+
+    K key;
+    E value;
+
+    public MapEntry (K key, E val) {
+        this.key = key;
+        this.value = val;
+    }
+
+    public int compareTo (K that) {
+        @SuppressWarnings("unchecked")
+        MapEntry<K,E> other = (MapEntry<K,E>) that;
+        return this.key.compareTo(other.key);
+    }
+
+    public String toString () {
+        return "(" + key + "," + value + ")";
+    }
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class CBHT<K extends Comparable<K>, E> {
+
+    private SLLNode<MapEntry<K,E>>[] buckets;
+
+    @SuppressWarnings("unchecked")
+    public CBHT(int m) {
+        buckets = (SLLNode<MapEntry<K,E>>[]) new SLLNode[m];
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    public SLLNode<MapEntry<K,E>> search(K targetKey) {
+        int b = hash(targetKey);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (targetKey.equals(((MapEntry<K, E>) curr.element).key))
+                return curr;
+        }
+        return null;
+    }
+
+    public void insert(K key, E val) {		// Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (key.equals(((MapEntry<K, E>) curr.element).key)) {
+                curr.element = newEntry;
+                return;
+            }
+        }
+        buckets[b] = new SLLNode<MapEntry<K,E>>(newEntry, buckets[b]);
+    }
+
+    public void delete(K key) {
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> pred = null, curr = buckets[b]; curr != null; pred = curr, curr = curr.succ) {
+            if (key.equals(((MapEntry<K,E>) curr.element).key)) {
+                if (pred == null)
+                    buckets[b] = curr.succ;
+                else
+                    pred.succ = curr.succ;
+                return;
+            }
+        }
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            for (SLLNode<MapEntry<K,E>> curr = buckets[i]; curr != null; curr = curr.succ) {
+                temp += curr.element.toString() + " ";
+            }
+            temp += "\n";
+        }
+        return temp;
+    }
+
+}
+
+public class main {
+    public static void main (String[] args) throws IOException {
+        CBHT<String,Integer> tabela = new CBHT<String,Integer>(300);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine().trim();
+        int maxFreq = Integer.MIN_VALUE;
+        String maxWord = "";
+        int n = word.length();
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<=n; j++){
+                String sub = word.substring(i,j);
+                if(tabela.search(sub) != null){
+                    int freq = tabela.search(sub).element.value;
+                    tabela.insert(sub, freq+1);
+                }else{
+                    tabela.insert(sub,1);
+                }
+                SLLNode<MapEntry<String, Integer>> cell = tabela.search(sub);
+                if(cell.element.value > maxFreq){
+                        maxFreq = cell.element.value;
+                        maxWord = cell.element.key;
+                } else if(cell.element.value == maxFreq){
+                    if(cell.element.key.length() >= maxWord.length()){
+                        if(cell.element.key.length() == maxWord.length()){
+                            int cmp = cell.element.key.compareTo(maxWord);
+                            if(cmp < 0) maxWord = cell.element.key;
+                        }else maxWord = cell.element.key;
+                    }
+                }
+            }
+        }
+        System.out.println(maxWord);
+
+    }
+}
